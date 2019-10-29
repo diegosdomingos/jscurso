00a533a0b60f19d72e6c98523c3ee89836760a62
diegosdomingos <43971125+diegosdomingos@users.noreply.github.com>
2019-10-28 22:28:08 -0300

    Inicio do projeto - Curso JS SENAC - Add Fontes

---
+++ funcoes/hello.js
@@ -0,0 +1,10 @@
+/**
+ * Exemplo de função simples
+ * @author diegosdomingos
+ */
+function helloWorld(){
+    let nome
+    nome = prompt('Qual é o seu nome?')
+    alert('Hello '+nome)
+
+}
---
+++ vincularJS/documento.js
@@ -0,0 +1,6 @@
+/**
+ * Vinculando um documento JS no html
+ * @author diegosdomingos
+ */
+
+ document.write('<h2>Hello World</h2>')
